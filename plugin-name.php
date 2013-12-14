47fc47bd9f0522708001d9211fe3e3cdef13f8bf
claudiosmweb <contato@claudiosmweb.com>
2013-12-14 21:07:15 -0200

    updated the main file

---
+++ plugin-name.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Use the PHP for https://github.com/tommcfarlin/WordPress-Plugin-Boilerplate
+ *
+ * @package   Plugin_Name
+ * @author    Your Name <email@example.com>
+ * @license   GPL-2.0+
+ * @link      http://example.com
+ * @copyright 2013 Your Name or Company Name
+ *
+ * @wordpress-plugin
+ * Plugin Name:       @TODO
+ * Plugin URI:        @TODO
+ * Description:       @TODO
+ * Version:           1.0.0
+ * Author:            @TODO
+ * Author URI:        @TODO
+ * Text Domain:       plugin-name-locale
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/<owner>/<repo>
+ */
